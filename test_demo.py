7c7550c771258b4aa771c9a71032bc13983b2574
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-08 02:27:02 +0700

    Skipping a test method examples

---
+++ test_demo.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from unittest import skip
+from unittest import SkipTest
+
+class TestDemo(TestCase):
+  def test_case_1(self):
+    self.assertEqual(1 + 1, 2)
+
+  @skip("Word in progress")
+  def test_case_2(self):
+    self.assertEqual(1 + 1, 2)
+
+  def test_case_3(self):
+    self.skipTest("Word in progress")
+
+  def test_case_4(self):
+    raise SkipTest("Word in progress")
